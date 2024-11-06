a3afdf1c0b81388b76b83220846a5ca03c30e7a3
DTA32 <97021335+DTA32@users.noreply.github.com>
2024-11-06 15:54:46 +0700

    moving helpers to util

--- helpers/utils.go
+++ helpers/utils.go
@@ -1,15 +1,18 @@
 package helpers
 
-import(
+import (
 	"bytes"
 	"fmt"
 	"github.com/fogleman/gg"
+	"github.com/go-resty/resty/v2"
 	gonanoid "github.com/matoous/go-nanoid/v2"
+	"github.com/pt-sinan-akbar/initializers"
 	"image/color"
 	"image/png"
 	"math/rand"
 	"time"
 )
+
 type ErrResponse struct {
 	Message string `json:"message"`
 }
@@ -45,4 +48,26 @@ func GenerateInitialsImage(name string) ([]byte, error) {
 		return nil, err
 	}
 	return buf.Bytes(), nil
+}
+
+func UploadFile(filename string, fileBytes []byte) error {
+	client := resty.New()
+	supabaseURL := initializers.ConfigSetting.SupabaseStorageURL
+	bucketName := initializers.ConfigSetting.SupabaseBucket
+	folderName := initializers.ConfigSetting.SupbaseFolder
+	fileName := filename
+	jwtToken := initializers.ConfigSetting.SupabaseSecretKey
+	resp, err := client.R().
+		SetHeader("Authorization", "Bearer "+jwtToken).
+		SetHeader("Content-Type", "image/jpeg").
+		SetBody(fileBytes).
+		Post(fmt.Sprintf("%s/storage/v1/object/%s/%s/%s", supabaseURL, bucketName, folderName, fileName))
+	fmt.Printf("##########: %s/storage/v1/object/%s/%s/%s\n", supabaseURL, bucketName, folderName, fileName)
+	if err != nil {
+		return fmt.Errorf("failed to upload image: %v", err)
+	}
+	if resp.StatusCode() != 200 && resp.StatusCode() != 201 {
+		return fmt.Errorf("failed to upload image: %v", resp.String())
+	}
+	return nil
 }
